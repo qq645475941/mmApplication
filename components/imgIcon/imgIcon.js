63939a0f6bda56662cdaa1b23d2d134013d600a0
qq645475941 <645475941@qq.com>
2019-08-20 20:47:50 +0800

    添加我的页面

---
+++ components/imgIcon/imgIcon.js
@@ -0,0 +1,34 @@
+// components/imgIcon.js
+Component({
+
+  options: {
+    addGlobalClass: true,
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    imgPath:{
+      type:String,
+      value:"",
+    },
+    text:{
+      type:String,
+      value:''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/my/index.js
+++ pages/my/index.js
@@ -5,6 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
+    orderInon:[
+      { imgPath: "/images/activity/activity.png", text: "全部",link: ""},
+      { imgPath: "/images/activity/activity.png", text: "待开始",link: ""},
+      { imgPath: "/images/activity/activity.png", text: "进行中",link: ""},
+      { imgPath: "/images/activity/activity.png", text: "已结束",link: ""},
+    ],
+    myInon:[
+      { imgPath: "/images/others/rili.png", text: "活动日历",link: ""},
+      { imgPath: "/images/others/shoucang.png", text: "活动收藏",link: ""},
+      { imgPath: "/images/others/headIcon.png", text: "我的关注",link: ""},
+      { imgPath: "/images/others/headIcon.png", text: "我的积分",link: ""},
+      { imgPath: "/images/others/scan.png", text: "商户入口",link: ""},
+      { imgPath: "/images/others/headIcon.png", text: "联系客服",link: ""},
+      { imgPath: "/images/others/scan.png", text: "扫描二维码",link: ""},
+      { imgPath: "/images/others/qiandao.png", text: "签到",link: ""},
+      { imgPath: "/images/others/shoucang.png", text: "关注公众号",link: ""},
+      { imgPath: "/images/others/headIcon.png", text: "业务合作",link: ""},
+      { imgPath: "/images/others/headIcon.png", text: "关于我们",link: ""},
+      { imgPath: "/images/others/qiandao.png", text: "意见反馈",link: ""},
+    ],
 
   },
 
